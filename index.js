7b98b15efa61b4edfb8c7f649a17e48218f77280
theEngMansour <mansour.yemen1@gmail.com>
2023-07-23 00:16:21 +0300

    add

--- index.js
+++ index.js
@@ -1,55 +1,12 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-import { createServer } from 'http';
-import { makeExecutableSchema } from '@graphql-tools/schema';
+import express from "express";
 
-// default settings for port & pubsub
-const PORT = 3000;
-
-// Schema definition
-const typeDefs = `#graphql
-  type Query {
-    currentNumber: Int
-  }
-
-`;
-
-// Resolver map
-const resolvers = {
-  Query: {
-    currentNumber() {
-      return 1;
-    },
-  },
-};
-
-// Create schema, which will be used separately by ApolloServer and
-// the WebSocket server.
-const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-// Create an Express app and HTTP server; we will attach the WebSocket
-// server and the ApolloServer to this HTTP server.
 const app = express();
-const httpServer = createServer(app);
+const port = 3000;
 
-// Set up ApolloServer.
-const server = new ApolloServer({
-  schema,
-  plugins: [
-    // Proper shutdown for the HTTP server.
-    ApolloServerPluginDrainHttpServer({ httpServer }),
-  ],
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-await server.start();
-app.use('/graphql', cors(), bodyParser.json(), expressMiddleware(server));
-
-// Now that our HTTP server is fully set up, actually listen.
-httpServer.listen(PORT, () => {
-  console.log(`🚀 Query endpoint ready at http://localhost:${PORT}/graphql`);
-
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
